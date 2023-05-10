a3d10a9b0776c8f779fc8615cf94aeb7ac94913a
Nguyen Phan Du <badpaybad@gmail.com>
2023-05-10 22:31:39 +0700

    _buildCamLocal

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,5 +1,10 @@
-
 import 'package:flutter/material.dart';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
+import 'dart:math' as math;
+import 'package:image/image.dart' as DartImage;
+import 'package:oktoast/oktoast.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -22,15 +27,101 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Map<CameraDescription, CameraController?> _camCtlMap = {};
+  CameraDescription? camera;
+
+  @override
+  void initState() {
+    super.initState();
+    init() async {
+      final cameras = await availableCameras();
+      for (var c in cameras) {
+        print("camera ${c.name}");
+        await _initCameraCtl(c);
+        //back camera index=0
+        break;
+      }
+    }
+
+    init().then((value) async{
+      if(mounted)setState(() {
+
+      });
     });
+  }
+
+  bool _initDoneLocalCam = false;
+
+  Future<void> _initCameraCtl(CameraDescription camera) async {
+    var _controller = _camCtlMap[camera];
+
+    _initDoneLocalCam = false;
+    this.camera = camera;
+    if (_controller == null) {
+      _controller = CameraController(
+        camera,
+        ResolutionPreset.ultraHigh,
+      );
+      await _controller!.initialize();
+      _camCtlMap[camera] = _controller;
+    }
+
+    _initDoneLocalCam = true;
+  }
+
+  DeviceOrientation _getApplicableOrientation() {
+    var _controller = _camCtlMap[this.camera];
+
+    print(_controller!.value.lockedCaptureOrientation);
+    print(_controller!.value.previewPauseOrientation);
+    return _controller!.value.isRecordingVideo
+        ? _controller!.value.recordingOrientation!
+        : (_controller!.value.previewPauseOrientation ??
+            _controller!.value.lockedCaptureOrientation ??
+            _controller!.value.deviceOrientation);
+  }
+
+  Widget _buildCamLocal() {
+    var controller = _camCtlMap[this.camera];
+
+    if (_initDoneLocalCam == false || controller == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    var vidOrient = _getApplicableOrientation();
+    var orient = MediaQuery.of(context).orientation;
+
+    if (vidOrient == DeviceOrientation.landscapeLeft &&
+        this.camera!.sensorOrientation == 0) {
+      // cause camera usb for rk3288
+      Widget tempCam = controller!.buildPreview();
+
+      tempCam = Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(math.pi),
+          child: tempCam);
+
+      //var screenW = MediaQuery.of(context).size.width;
+      var screenH = MediaQuery.of(context).size.height;
+      var vidW = controller!.value.previewSize!.width;
+      var vidH = controller!.value.previewSize!.height;
+      var newVidH = screenH - 100; // vidH/2;
+      var newVidW = newVidH * vidW / vidH;
+
+      // print("orient ${orient.toString()} $vidOrient");
+      // print("vidW $vidW vidH $vidH");
+      // print("screenW $screenW screenH $screenH");
+      // print("newVidW $newVidW newVidH $newVidH");
+
+      return SizedBox(
+        width: newVidW,
+        height: newVidH,
+        child: tempCam,
+      );
+    }
+
+    Widget tempCam = CameraPreview(controller!);
+    return tempCam;
   }
 
   @override
@@ -67,8 +158,10 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            SizedBox(
+              width: 120,
+              height: 120,
+              child: _buildCamLocal(),
             ),
             Text(
               '$_counter',
@@ -78,7 +171,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
